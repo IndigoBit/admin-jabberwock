1b2e0d9da2eb37cf8f30dcc1b54237a80d465e46
Nick Freitas <nickfrei@gmail.com>
2019-04-19 06:00:27 -0400

    move admin frontend to root and remove api

---
+++ src/app/salvage/oranization-detail-article-list/oranization-detail-article-list.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { OranizationDetailArticleListComponent } from './oranization-detail-article-list.component';
+
+// describe('OranizationDetailArticleListComponent', () => {
+//   let component: OranizationDetailArticleListComponent;
+//   let fixture: ComponentFixture<OranizationDetailArticleListComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ OranizationDetailArticleListComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(OranizationDetailArticleListComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ src/app/salvage/oranization-detail-article-list/oranization-detail-article-list.component.ts
@@ -0,0 +1,14 @@
+// import { Component, OnInit, Input } from "@angular/core";
+// import { Article } from "../gql-schemas/article.gql-schema";
+
+// @Component({
+//   selector: "app-oranization-detail-article-list",
+//   templateUrl: "./oranization-detail-article-list.component.html",
+//   styleUrls: ["./oranization-detail-article-list.component.scss"]
+// })
+// export class OranizationDetailArticleListComponent implements OnInit {
+//   @Input() data: Article[];
+//   constructor() {}
+
+//   ngOnInit() {}
+// }
